3c7020ab7d6a888deb6283a61176fde9a6cde9ac
GanZX666 <3061235233@qq.com>
2022-11-06 21:52:12 +0800

    第一次测试

--- Assets/Scripts/GitHubTest.cs
+++ Assets/Scripts/GitHubTest.cs
@@ -1,8 +1,17 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GitHubTest : MonoBehaviour
 {
-    
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        print("GitHub测试");
+    }
 }
